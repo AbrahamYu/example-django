eba848651e94b2722f8e23906dc855dede4428c7
Ubuntu <ubuntu@ip-172-31-15-138.ap-northeast-2.compute.internal>
2023-01-06 04:46:49 +0000

    django nginx docker 확인

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM python:3.8-slim-buster
 
-WORKDIR /src
-
 ENV PYTHONUNBUFFERED 1
 ENV C_FORCE_ROOT true
 
 RUN mkdir /config
-
 ADD /config/requirements.txt /config/
 
+RUN python -m pip install --upgrade pip
+RUN pip install --root-user-action=ignore requests
 RUN pip install -r /config/requirements.txt
-RUN pip install --upgrade pip
+
 RUN mkdir /src;
+
+WORKDIR /src
--- src/project/settings.py
+++ src/project/settings.py
@@ -75,13 +75,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'db',
-        'PORT': '5432',  # 5432는 PostgreSQL의 기본포트이다
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -122,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
